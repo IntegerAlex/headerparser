acafaea7363aecb5ea20244a472f2609b5581a96
IntegerAlex <inquiry.akshatkotpalliwar@gmail.com>
2023-08-25 21:50:27 +0530

    headerpraser

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res) {
+  const ipAddress = req.ip;
+  const preferredLanguage = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  const userInfo = {
+    ipaddress: ipAddress,
+    language: preferredLanguage,
+    software: software
+  };
+  res.json(userInfo);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
